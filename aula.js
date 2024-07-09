10b70efdbc6d0ffb4c6e8bfcf57e0b432a1b8c24
gustavo0867 <gbrasilreis08@gmail.com>
2024-07-09 00:09:31 -0400

    Subindo anotações

---
+++ aula.js
@@ -0,0 +1,48 @@
+/*var teste;
+
+teste = 'ola amigo'
+
+console.log(teste)
+
+for(i=0; i<3; i++){
+    
+
+    if(i==2){
+    console.log('vamos dominar saporra')
+    }else console.log(i)
+}
+
+let x=10, y=12
+
+
+
+const testando = (a,b) => {
+
+    var total=a+b;
+
+    console.log('a soma é: '+total)
+}
+testando(x,y);
+
+*/
+
+var mediamoto = 36
+
+var viagem = 380
+
+
+gastoviagem(viagem,mediamoto)
+
+
+function gastoviagem(distancia,media){
+
+    let x=distancia/media 
+
+    console.log('O valor gasto será de: '+(x*5.98))
+
+}
+olha so 
+
+console.log(viagem)
+
+

14d054b3f805e01d0af332c8aa4802c505edcf6c
gustavo0867 <gbrasilreis08@gmail.com>
2024-07-23 23:05:17 -0400

    Objetos

---
+++ objetos.js
@@ -0,0 +1,15 @@
+const gusta = {
+    nome: "Gugu",
+    idade: 25
+}
+
+console.log(gusta.idade)
+
+gusta.teste = "ok";
+
+console.log(gusta)
+
+
+gusta['nome'] = "lindo"
+
+console.log(gusta.nome)
